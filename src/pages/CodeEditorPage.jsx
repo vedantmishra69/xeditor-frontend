38263d0d7ece45c302666ccadc43249c4b20d30d
Vedant Mishra <vedantmishra050302@gmail.com>
2024-11-01 17:22:48 +0530

    changed stdout + stderr -> output

--- src/pages/CodeEditorPage.jsx
+++ src/pages/CodeEditorPage.jsx
@@ -5,29 +5,39 @@ import { LANGUAGE_MAPPING } from "../lib/constants";
 import { fetchResult, submitCode } from "../lib/util";
 
 const CodeEditorPage = () => {
-  const [stdin, setStdin] = useState("");
-  const [stdout, setStdout] = useState("");
-  const [stderr, setStderr] = useState("");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [time, setTime] = useState("");
+  const [memory, setMemory] = useState("");
   const { language, setLanguage, sourceCode, setSourceCode } = useCodeContext();
 
-  const handleStdin = (e) => setStdin(e.target.value);
+  const handleInput = (e) => setInput(e.target.value);
   const handleLanguage = (name) => {
     setLanguage(name);
-    setStdin("");
-    setStdout("");
-    setStderr("");
+    setInput("");
+    setOutput("");
     setSourceCode("");
+    setTime("");
+    setMemory("");
   };
+
   const handleSubmit = async () => {
     if (sourceCode) {
-      setStdout("Loading...");
-      const data = await submitCode(language, sourceCode, stdin);
+      setOutput("Loading...");
+      setTime("");
+      setMemory("");
+      const data = await submitCode(language, sourceCode, input);
       if (!data.error && data.token) {
         const result = await fetchResult(data.token);
         if (result) {
-          setStdout(result.stdout);
-          setStderr(result.stderr);
-        } else setStdout("Error");
+          setOutput(
+            `${result.stdout + "\n"}${
+              result.compile_output || result.stderr + "\n"
+            }Program finished with exit code ${result.exit_code}.`
+          );
+          setTime(result.time);
+          setMemory(result.memory);
+        }
       }
     }
   };
@@ -42,85 +52,60 @@ const CodeEditorPage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8 flex flex-row justify-between">
-      <div className="w-full flex justify-center m-4">
+    <div className="min-h-screen bg-gray-100 flex flex-row justify-between">
+      <div className="flex-1 flex justify-center p-4">
         <EditorComponent />
       </div>
-      <div className="w-full flex-col content-center">
-        <div className="w-full max-w-3xl mb-4">
-          <label
-            className="block text-gray-700 font-semibold mb-2"
-            htmlFor="stdin"
-          >
-            Stdin
-          </label>
-          <textarea
-            id="stdin"
-            value={stdin}
-            onChange={handleStdin}
-            className="w-full h-20 p-4 bg-white border border-gray-300 rounded-lg resize-none"
-            placeholder="Provide input if needed..."
-          ></textarea>
+      <div className="flex-1 h-screen flex flex-col p-4">
+        <div className="flex-1 space-y-4 flex flex-col mb-2">
+          <div className="flex-1 w-full max-w-3xl flex flex-col">
+            <label className="block text-gray-700 font-semibold mb-2">
+              Input
+            </label>
+            <textarea
+              value={input}
+              onChange={handleInput}
+              className="w-full flex-1 p-2 bg-white border border-gray-300 resize-none"
+              placeholder="Provide input if needed..."
+            ></textarea>
+          </div>
+          <div className="flex-1 w-full max-w-3xl flex flex-col">
+            <label className="block text-gray-700 font-semibold mb-2">
+              Output
+            </label>
+            <textarea
+              value={output}
+              className="w-full flex-1 p-2 bg-white border border-gray-300 resize-none"
+              placeholder="Output will be displayed here..."
+              readOnly
+            ></textarea>
+          </div>
         </div>
-
-        <div className="w-full max-w-3xl mb-4">
-          <label
-            className="block text-gray-700 font-semibold mb-2"
-            htmlFor="stdout"
-          >
-            Stdout
-          </label>
-          <textarea
-            id="stdout"
-            value={stdout}
-            className="w-full h-20 p-4 bg-white border border-gray-300 rounded-lg resize-none"
-            placeholder="Output will be displayed here..."
-            readOnly
-          ></textarea>
+        <div className="w-full flex flex-row p-2 gap-2">
+          <div>{time ? `Time: ${time}s` : ""}</div>
+          <div>{memory ? `Memory: ${memory}kB` : ""}</div>
         </div>
-
-        <div></div>
-
-        <div className="w-full max-w-3xl mb-4">
-          <label
-            className="block text-gray-700 font-semibold mb-2"
-            htmlFor="stderr"
-          >
-            Stderr
-          </label>
-          <textarea
-            id="stderr"
-            value={stderr}
-            className="w-full h-20 p-4 bg-white border border-gray-300 rounded-lg resize-none"
-            placeholder="Error will be displayed here..."
-            readOnly
-          ></textarea>
-        </div>
-
-        <div className="w-full max-w-3xl mb-4">
-          <label
-            className="block text-gray-700 font-semibold mb-2"
-            htmlFor="language"
-          >
-            Language
-          </label>
-          <select
-            id="language"
-            defaultValue={language}
-            className="w-full p-3 bg-white border border-gray-300 rounded-lg"
-            required
-          >
-            {languageOptions}
-          </select>
-        </div>
-
-        <div className="w-full max-w-3xl flex justify-end">
-          <button
-            onClick={handleSubmit}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-lg"
-          >
-            Submit
-          </button>
+        <div className="mt-auto w-full max-w-3xl">
+          <div className="mb-4">
+            <label className="block text-gray-700 font-semibold mb-2">
+              Language
+            </label>
+            <select
+              defaultValue={language}
+              className="w-full p-3 bg-white border border-gray-300 rounded-lg"
+              required
+            >
+              {languageOptions}
+            </select>
+          </div>
+          <div className="flex justify-end">
+            <button
+              onClick={handleSubmit}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-lg"
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </div>
