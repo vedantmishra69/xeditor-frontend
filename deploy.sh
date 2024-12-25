14d53ca3adbbfd1800d949f57e1ba1f36299776b
Vedant Mishra <vedantmishra050302@gmail.com>
2024-12-25 16:29:11 +0530

    small thing

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker stop xeditor-frontend
 docker rm xeditor-frontend
 docker image rm xeditor-frontend
 docker build -t xeditor-frontend .
-docker run --name xeditor-frontend -d -p 8000:8000 xeditor-frontend
+docker run --name xeditor-frontend -d -p 80:80 xeditor-frontend
 
