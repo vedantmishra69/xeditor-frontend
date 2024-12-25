a9ff2bba8a635af48a6840530c9344199c7f694b
Vedant Mishra <vedantmishra050302@gmail.com>
2024-12-26 00:02:49 +0530

    port change

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker stop xeditor-frontend
 docker rm xeditor-frontend
 docker image rm xeditor-frontend
 docker build -t xeditor-frontend .
-docker run --name xeditor-frontend -d -p 80:80 xeditor-frontend
+docker run --name xeditor-frontend -d -p 3000:80 xeditor-frontend
 
