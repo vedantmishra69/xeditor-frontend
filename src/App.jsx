345a2812d56b43c23b738bc3a02532d21d8f6807
Vedant Mishra <vedantmishra050302@gmail.com>
2024-12-29 00:17:56 +0530

    pop notif theme changed

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,33 @@ function App() {
   return (
     <>
       <div>
-        <Toaster />
+        <Toaster
+          position="bottom-left"
+          reverseOrder={false}
+          duration={5000}
+          toastOptions={{
+            className: "",
+            style: {
+              background: "#282A36",
+              border: "2px solid #6272A4",
+              color: "#F8F8F2",
+              fontSize: "1.125rem",
+              lineHeight: "1.75rem",
+            },
+            success: {
+              iconTheme: {
+                primary: "#6272A4",
+                secondary: "#282A36",
+              },
+            },
+            error: {
+              iconTheme: {
+                primary: "#BE609A",
+                secondary: "#282A36",
+              },
+            },
+          }}
+        />
       </div>
       <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
         <AuthContext>
